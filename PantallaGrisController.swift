75dd5dd3b7e29ad340b3db9e21ccb0ad6b559bb9
Alumno <ulsa@MARCO-ANTONIO.local>
2019-08-23 14:15:36 -0700

    Practica terminada

---
+++ PantallaGrisController.swift
@@ -0,0 +1,17 @@
+//
+//  PantallaGrisController.swift
+//  Sonidos
+//
+//  Created by Alumno on 23/08/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PantallaRojaController : UIViewController {
+    
+    @IBAction func doTapRegresar(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
--- Sonidos/ViewController.swift
+++ Sonidos/ViewController.swift
@@ -13,19 +13,41 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player : AVAudioPlayer?
+    var playerMusicaFondo : AVAudioPlayer?
+    
+    var reproduccion = false
 
     @IBAction func doTapReproducir(_ sender: Any) {
+        if reproduccion {
+            let pathMusicaFondo = Bundle.main.path(forResource: "Shoop (Casual Connection Rework).wav", ofType: nil)
+            let urlMusicaFondo = URL(fileURLWithPath: pathMusicaFondo!)
+            do {
+                player = try AVAudioPlayer(contentsOf: urlMusicaFondo)
+            }catch {
+                
+            }
+        }else {
+            reproduccion = true
+        }
+        
+        
         if player != nil{
             player?.play()
+            
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        let path = Bundle.main.path(forResource: "Audio 29.wav", ofType: nil)
+        let path = Bundle.main.path(forResource: "AUDIO 29.wav", ofType: nil)
         let url = URL(fileURLWithPath: path!)
+        
+        let pathMusicaFondo = Bundle.main.path(forResource: "Shoop (Casual Connection Rework).wav", ofType: nil)
+        let urlMusicaFondo = URL(fileURLWithPath: pathMusicaFondo!)
         
         do {
             player = try AVAudioPlayer(contentsOf: url)
+            playerMusicaFondo = try AVAudioPlayer(contentsOf: urlMusicaFondo)
+            //playerMusicaFondo?.play()
         } catch {
             
         }
